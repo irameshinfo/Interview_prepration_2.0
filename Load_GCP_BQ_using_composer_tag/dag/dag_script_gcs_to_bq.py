f17b7b4166858041ab7858f718dc279f7b67800e
irameshinfo <irameshinfo@gmail.com>
2026-04-19 11:07:08 +0530

    test

---
+++ Load_GCP_BQ_using_composer_tag/dag/dag_script_gcs_to_bq.py
@@ -0,0 +1,25 @@
+from airflow import DAG
+from datetime import datetime
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
+
+with DAG(
+    dag_id="gcs_to_bq_emp_load",
+    start_date=datetime(2024, 1, 1),
+    schedule=None,
+    catchup=False,
+    tags=["gcp", "bigquery", "csv-load"],  # ✅ Tags defined here
+) as dag:
+
+    load_csv_to_bq = GCSToBigQueryOperator(
+        task_id="load_emp_csv",
+        bucket="pythonscriptload",
+        source_objects=["emp.csv"],
+        destination_project_dataset_table="ranjanrishi-project.python_script_load.emp_air_flow",
+        source_format="CSV",
+        skip_leading_rows=1,
+        autodetect=True,
+        write_disposition="WRITE_TRUNCATE",
+        gcp_conn_id="google_cloud_default",
+    )
+
+    load_csv_to_bq
