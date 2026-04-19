2b5ad156d3fb9cb7aeb1da7e9902fe3ab50a1475
irameshinfo <irameshinfo@gmail.com>
2026-04-19 11:46:26 +0530

    test

---
+++ Load_data_using_cloud_function/cloud_function/main.py
@@ -0,0 +1,34 @@
+from google.cloud import bigquery
+
+def load_csv_to_bq(event, context):
+    client = bigquery.Client()
+
+    bucket_name = event['bucket']
+    file_name = event['name']
+
+    # Optional: process only specific folder/file
+    if not file_name.endswith(".csv"):
+        print("Skipping non-CSV file")
+        return
+
+    uri = f"gs://{bucket_name}/{file_name}"
+
+    table_id = "ranjanrishi-project.cloud_function_load.emp"
+
+    job_config = bigquery.LoadJobConfig(
+        source_format=bigquery.SourceFormat.CSV,
+        field_delimiter=",",           # 👈 delimiter here
+        skip_leading_rows=1,
+        autodetect=True,
+        write_disposition="WRITE_TRUNCATE"  # or WRITE_APPEND
+    )
+
+    load_job = client.load_table_from_uri(
+        uri,
+        table_id,
+        job_config=job_config
+    )
+
+    load_job.result()  # Wait for job to complete
+
+    print(f"Loaded {file_name} into {table_id}")
